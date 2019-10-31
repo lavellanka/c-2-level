64af8ac0c5b570babc86641f326e87fafcd4340c
lavellanka <mrs.svirgstina@gmail.com>
2019-10-31 08:01:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+//Свиргстина Александра
+
+//1. Добавить свои объекты в иерархию объектов, чтобы получился красивый задний фон,
+//     похожий на полёт в звёздном пространстве.
+//2. * Заменить кружочки картинками, используя метод DrawImage.
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form1 form = new Form1
+            {
+                Width = Screen.PrimaryScreen.Bounds.Width,
+                Height = Screen.PrimaryScreen.Bounds.Height
+            };                      
+
+            try
+            {
+                if (form.Width > 1000 || form.Width < 0 || form.Height > 1000 || form.Height < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(form.Width + " " + form.Height);
+                Console.WriteLine(e.Message);
+            }
+
+            Game game = new Game();
+            Game.Init(form);
+            form.Show();
+            game.Load();
+            Game.Draw();
+            Application.Run(form);
+        }        
+    }
+}
